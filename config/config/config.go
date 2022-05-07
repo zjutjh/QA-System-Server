69ac878cf53a4e35545da0b42cab22d697fba6e8
Patrick-Star-CN <1592810043@qq.com>
2022-05-07 08:35:46 +0800

    fix: fix some bug of config

--- config/config/config.go
+++ config/config/config.go
@@ -11,7 +11,7 @@ func init() {
 	Config.SetConfigName("config")
 	Config.SetConfigType("yaml")
 	Config.AddConfigPath(".")
-	Config.WriteConfig()
+	Config.WatchConfig()
 	err := Config.ReadInConfig()
 	if err != nil {
 		log.Fatal("Config not find", err)
--- config/database/database.go
+++ config/database/database.go
@@ -12,13 +12,13 @@ var DB *gorm.DB
 
 func Init() {
 
-	user := config.Config.GetString("database.user")
-	pass := config.Config.GetString("database.pass")
+	username := config.Config.GetString("database.username")
+	password := config.Config.GetString("database.password")
 	port := config.Config.GetString("database.port")
 	host := config.Config.GetString("database.host")
 	name := config.Config.GetString("database.name")
 
-	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local", user, pass, host, port, name)
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local", username, password, host, port, name)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		DisableForeignKeyConstraintWhenMigrating: true, // 关闭外键约束 提升数据库速度
 	})
